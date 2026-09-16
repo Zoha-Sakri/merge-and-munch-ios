a63c1d94ed423a490a1c8601e09cba2f6a43cfb2
Zoha <zohasakri@gmail.com>
2026-09-17 00:45:04 +0530

    Add generated Xcode project and native polish

--- Sources/MergeAndMunch/MergeAndMunchApp.swift
+++ Sources/MergeAndMunch/MergeAndMunchApp.swift
@@ -3,5 +3,5 @@ import SwiftUI
 @main
 struct MergeAndMunchApp: App {
     @StateObject private var game = GameStore()
-    var body: some Scene { WindowGroup { ContentView().environmentObject(game) } }
+    var body: some Scene { WindowGroup { ContentView().environmentObject(game).environment(\.font, .system(size: 16, design: .rounded)) } }
 }
